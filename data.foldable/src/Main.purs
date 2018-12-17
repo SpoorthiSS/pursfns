f6361fd46082960693715c787035261572a72dfd
Spoorthi <spoorthi@Juspays-MacBook-Air-5.local>
2018-12-17 17:13:52 +0530

    fold function added

--- data.foldable/src/Main.purs
+++ data.foldable/src/Main.purs
@@ -7,6 +7,10 @@ import Data.Foldable
 -- import Data.Array
 import Data.Maybe
 
+
+
+
+
 -- foldrDefault1 :: forall f a b. Foldable f => (a -> b -> b) -> b -> f a -> b
 -- foldrDefault1 fnc i f = foldrDefault fnc i f
 -- main = logShow (foldrDefault (+) 1 [1,2,3])
@@ -14,5 +18,13 @@ import Data.Maybe
 
 -- main = logShow (foldMapDefaultL (\x -> [Just(x+1)]) [1,2,3])
 
-main = logShow( foldM (\x -> \y -> Just (x+y)) 1 [1,2,3])
+-- main = logShow( foldM (\x -> \y -> Just (x+y)) 1 [1,2,3])
+
+-- main = logShow(intercalate "!" ["abc","xyz", "lmn"])
+
+-- main = logShow(surround "*" ["1", "2"]) 
+
+-- main = logShow(find (\x -> elem x [1,2,3]) [1,2,3])
+
+main = logShow (fold [Just [2,3] , Just [4]])
 
